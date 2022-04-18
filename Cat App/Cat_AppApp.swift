fec08377ed7bece643a7c0be1725e09870daf374
TheLoneAngel <98775684+TheLoneAngel@users.noreply.github.com>
2022-04-18 14:27:29 -0500

    files

---
+++ Cat App/Cat_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cat_AppApp.swift
+//  Cat App
+//
+//  Created by 64016200 on 2/15/22.
+//
+
+import SwiftUI
+
+@main
+struct Cat_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cat App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cat App
+//
+//  Created by 64016200 on 2/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
